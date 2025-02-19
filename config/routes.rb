f50726e08988f325ad5b7720f5ba218a708f6fee
AakashB-Sky <62520487+AakashB-Sky@users.noreply.github.com>
2025-02-19 02:06:13 +0000

    add_item action complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
 
   get("/", { :controller => "items", :action => "index" })
-  get("/backdoor", { :controller => "backdoor", :action => "add_item" })
+  get("/backdoor", { :controller => "backdoor", :action => "add_item_form" })
 end
