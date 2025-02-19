4806d7fea665511f68c8e9db7058a43f08d61ab9
AakashB-Sky <62520487+AakashB-Sky@users.noreply.github.com>
2025-02-19 02:11:13 +0000

    done!

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
 
   get("/", { :controller => "items", :action => "index" })
   get("/backdoor", { :controller => "backdoor", :action => "add_item_form" })
+  post("/backdoor/add_item", { :controller => "backdoor", :action => "add_item" })
 end
